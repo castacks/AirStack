611a2010b2232158f676993aaa4a761c3b464fe4
John Keller <jkeller2@andrew.cmu.edu>
2024-06-20 13:32:35 -0400

    added packages for the robot and px4 interfaces

---
+++ robot/autonomy/controls/robot_interface/src/robot_interface_node.cpp
@@ -0,0 +1,22 @@
+#include <pluginlib/class_loader.hpp>
+#include <robot_interface/robot_interface.hpp>
+
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("robot_interface_node");
+  
+  pluginlib::ClassLoader<robot_interface::RobotInterface> loader("robot_interface", "robot_interface::RobotInterface");
+  
+  try{
+    std::shared_ptr<robot_interface::RobotInterface> ri = loader.createSharedInstance("px4_interface::PX4Interface");
+    ri->initialize(node);
+    
+    ri->request_control();
+  }
+  catch(pluginlib::PluginlibException& ex){
+    printf("The plugin failed to load for some reason. Error: %s\n", ex.what());
+  }
+  
+  return 0;
+}
