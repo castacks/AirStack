f1c0dfcd4960df6aacf450c6a2c814a4c2c90d29
Andrew Jong <ajong@andrew.cmu.edu>
2024-06-25 16:17:38 -0400

    Add convenient launch script for Ascent SITL

---
+++ simulation/AscentAeroSystemsSITLPackage/launch_ascent_sitl.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+# kill all child processes on interrupt
+trap 'pkill -P $$; exit' SIGINT SIGTERM
+
+ASCENT_SITL_PORT=14552  # port to talk to QGC
+ISAAC_SIM_PORT=14553    # port for our autonomy stack
+
+# launch QGC, SITL, and mavproxy
+./AscentQLinux/AscentQ & \
+./spirit_sitl -S --model coaxial -I0 & \
+\
+mavproxy.py --streamrate=-1 --master tcp:127.0.0.1:5760 \
+    --out udp:127.0.0.1:$ASCENT_SITL_PORT \
+    --out udp:127.0.0.1:$ISAAC_SIM_PORT & \
+
+wait
