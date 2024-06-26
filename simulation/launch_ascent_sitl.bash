9993b4f6a2bb965770ea94b3a9e31f6a6cf9f70b
Andrew Jong <ajong@andrew.cmu.edu>
2024-06-26 14:55:44 -0400

    Move ascent launch out of main dir

--- simulation/launch_ascent_sitl.bash
+++ simulation/launch_ascent_sitl.bash
@@ -1,15 +1,20 @@
 #!/bin/bash
+# Author: Andrew Jong (ajong@andrew.cmu.edu)
 set -ex
 
 # kill all child processes on interrupt
 trap 'pkill -P $$; exit' SIGINT SIGTERM
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ASCENT_DIR="$SCRIPT_DIR/AscentAeroSystemsSITLPackage"
+
 ASCENT_SITL_PORT=14552  # port to talk to QGC
 ISAAC_SIM_PORT=14553    # port for our autonomy stack
 
-# launch QGC, SITL, and mavproxy
-./AscentQLinux/AscentQ & \
-./spirit_sitl -S --model coaxial -I0 & \
+
+# launch QGC, SITL, and mavproxy as child processes
+$ASCENT_DIR/AscentQLinux/AscentQ & \
+$ASCENT_DIR/spirit_sitl -S --model coaxial -I0 & \
 \
 mavproxy.py --streamrate=100 --master tcp:127.0.0.1:5760 \
     --out udp:127.0.0.1:$ASCENT_SITL_PORT \
