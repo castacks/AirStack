4a5eb3c6eafc4774af6e2eea49be27b9a04c8bf1
Andrew Jong <ajong@andrew.cmu.edu>
2024-07-01 17:39:30 -0400

    Cleanup

---
+++ ros_ws/src/robot/autonomy/controls/launch/launch_controls.py
@@ -0,0 +1,12 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='mavros',
+            node_namespace='mavros',
+            node_executable='apm.launch',
+            node_name='mavros_ardupilot'
+        )
+    ])
