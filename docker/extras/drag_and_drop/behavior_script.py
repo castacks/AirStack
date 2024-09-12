7688468581233dcb90f84e49d419852dee31f0fd
jfkeller <jkeller2@andrew.cmu.edu>
2024-09-12 16:58:40 -0400

    implemented ascent omnigraph node (#35)

--- docker/extras/drag_and_drop/behavior_script.py
+++ docker/extras/drag_and_drop/behavior_script.py
@@ -102,15 +102,16 @@ def start_server(host='127.0.0.1', port=65432):
             
 class TestScript(BehaviorScript):
     def on_init(self):
-        print('PRIM PATH', self.prim_path)
+        print('on init')
+        #print('PRIM PATH', self.prim_path)
         self.p = self.stage.GetPrimAtPath(self.prim_path)
         self.prop_prim = self.stage.GetPrimAtPath(str(self.prim_path) + '/spirit_uav/meshes/mesh_17')
         #print('YOOOOOOOOOOOO', str(self.prim_path) + '/spirit_uav/meshes/mesh_17', self.prop_prim)
-        print(self.p.GetAttributes())
+        #print(self.p.GetAttributes())
         self.p.GetAttribute('xformOp:translate').Set((0,0, 0))
         self.q_type = type(self.p.GetAttribute("xformOp:orient").Get())
         self.p.GetAttribute('xformOp:orient').Set(self.p.GetAttribute("xformOp:orient").Get())
-        print(self.p.GetAttribute("xformOp:translate").Get())
+        #print(self.p.GetAttribute("xformOp:translate").Get())
         self.dronekit_connection = None
         self.initialized = False
         self.drone_count = 0
@@ -118,10 +119,11 @@ class TestScript(BehaviorScript):
         self.prop_rotation_q = Quatf(0.9999619, 0, 0.0087265, 0) # 1 degree
         for i in range(7):
             self.prop_rotation_q *= self.prop_rotation_q
-        print(self.prop_prim.GetAttribute('xformOp:orient'))
+        #print(self.prop_prim.GetAttribute('xformOp:orient'))
         self.prop_prim.GetAttribute('xformOp:orient').Set(self.prop_prim.GetAttribute("xformOp:orient").Get()*
                                                           self.prop_rotation_q)
         #'''
+        print('on init done')
 
     def on_update(self, current_time, delta_time):
         #print('update', current_time, delta_time)
@@ -168,7 +170,8 @@ class TestScript(BehaviorScript):
                                                                   self.prop_rotation_q)
                 #'''
             else:
-                print("Drone location from dronekit is None")
+                #print("Drone location from dronekit is None")
+                pass
     
     def on_destroy(self):
         print('destroy')
