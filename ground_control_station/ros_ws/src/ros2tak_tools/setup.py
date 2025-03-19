9e3cd29df5b8da88414cb1031f7c4d1ef549b6d2
adi <rauniyar@cmu.edu>
2025-03-19 17:30:54 -0400

    adding tak_helper.py

--- ground_control_station/ros_ws/src/ros2tak_tools/setup.py
+++ ground_control_station/ros_ws/src/ros2tak_tools/setup.py
@@ -7,7 +7,7 @@ package_name = 'ros2tak_tools'
 setup(
     name=package_name,
     version='1.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(exclude=['test']) + ['tak_helper'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
