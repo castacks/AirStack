47c9fcc0f7c084be01bb4305d866cf003d8c7f71
John Keller <jkeller2@andrew.cmu.edu>
2025-03-26 15:49:37 -0400

    added control panel to gui for interfacing with docker and rosbag recording

--- ground_control_station/ros_ws/src/rqt_airstack_control_panel/src/rqt_airstack_control_panel/template.py
+++ ground_control_station/ros_ws/src/rqt_airstack_control_panel/src/rqt_airstack_control_panel/template.py
@@ -41,6 +41,9 @@ import python_qt_binding.QtGui as QtGui
 import python_qt_binding.QtCore as QtCore
 
 import subprocess
+import threading
+import time
+import copy
 
 try:
     from rqt_py_console.spyder_console_widget import SpyderConsoleWidget
@@ -49,6 +52,11 @@ except ImportError:
     _has_spyderlib = False
 
 
+logger = None
+
+def xor_encrypt_decrypt(data, key=983476):
+    return ''.join(chr(ord(c) ^ key) for c in data)
+    
 class InfoConfigDialog(qt.QDialog):
     def __init__(self, settings):
         super().__init__()
@@ -67,6 +75,13 @@ class InfoConfigDialog(qt.QDialog):
         self.username_entry = qt.QLineEdit()
         self.username_entry.setText(settings['username'])
         layout.addWidget(self.username_entry)
+
+        self.password_label = qt.QLabel('Password:')
+        layout.addWidget(self.password_label)
+        self.password_entry = qt.QLineEdit()
+        self.password_entry.setEchoMode(qt.QLineEdit.Password)
+        self.password_entry.setText(settings['password'])
+        layout.addWidget(self.password_entry)
         
         self.hostname_label = qt.QLabel('Hostname:')
         layout.addWidget(self.hostname_label)
@@ -91,6 +106,10 @@ class InfoConfigDialog(qt.QDialog):
         self.services_entry = qt.QLineEdit()
         self.services_entry.setText(', '.join(settings['excluded_services']))
         layout.addWidget(self.services_entry)
+
+        self.enable_display_checkbox = qt.QCheckBox('Enable Display')
+        self.enable_display_checkbox.setChecked(settings['enable_display'])
+        layout.addWidget(self.enable_display_checkbox)
         
         self.submit_button = qt.QPushButton('Submit')
         self.submit_button.clicked.connect(self.submit)
@@ -104,29 +123,46 @@ class InfoConfigDialog(qt.QDialog):
         name = self.name_entry.text()
         hostname = self.hostname_entry.text()
         username = self.username_entry.text()
+        password = self.password_entry.text()
         namespace = self.namespace_entry.text()
         path = self.path_entry.text()
         services = list(map(lambda x:x.strip(), self.services_entry.text().split(',')))
-        services.remove('')
-        self.result = {'name': name, 'username': username, 'hostname': hostname, 'namespace': namespace, 'path': path, 'excluded_services': services}
+        try:
+            services.remove('')
+        except:
+            pass
+        enable_display = self.enable_display_checkbox.isChecked()
+        self.result = {'name': name, 'username': username, 'password': password, 'hostname': hostname, 'namespace': namespace, 'path': path, 'excluded_services': services, 'enable_display': enable_display}
         self.accept()
 
 class CommandThread(QtCore.QThread):
     output_signal = QtCore.pyqtSignal(str)
     
-    def __init__(self, command):
+    def __init__(self, command, callback=None, wait_until_finished=False, timeout=None):
         super().__init__()
         self.command = command
+        if callback != None:
+            self.output_signal.connect(callback)
+        self.wait_until_finished = wait_until_finished
+        self.timeout = timeout
         self.running = True
+        self.start()
 
     def run(self):
-        process = subprocess.Popen(self.command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-        while self.running:
-            line = process.stdout.readline()
-            if not line:
-                break
-            self.output_signal.emit(line.strip())
+        process = subprocess.Popen(self.command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+        
+        if not self.wait_until_finished:
+            while self.running:
+                line = process.stdout.readline()
+                if not line:
+                    break
+                self.output_signal.emit(line.strip())
+        if self.wait_until_finished:
+            try:
+                process.wait(timeout=self.timeout)
+                self.output_signal.emit(process.stdout.read())
+            except subprocess.TimeoutExpired:
+                process.kill()
 
         process.terminate()
 
@@ -136,13 +172,21 @@ class CommandThread(QtCore.QThread):
 
 
 class InfoWidget(qt.QWidget):
-    def __init__(self, settings={'name': 'Name', 'username': 'airlab', 'hostname': 'localhost', 'namespace': 'none', 'path': '~/airstack', 'excluded_services': []}):
+    def __init__(self, settings={'name': 'Name', 'username': 'airlab', 'password': 'passme24', 'hostname': 'localhost', 'namespace': 'none', 'path': '~/airstack', 'excluded_services': [], 'enable_display': False}):
         super(qt.QWidget, self).__init__()
         self.layout = qt.QVBoxLayout(self)
-
+        #self.setObjectName('info_widget')
+        #self.setStyleSheet('#info_widget {border: 2px solid black;}')
+        
+        self.setAttribute(QtCore.Qt.WA_StyledBackground, True)
+        self.setObjectName('info_widget')
+        self.stylesheet = 'border: 2px solid lightgrey;  border-radius: 10px;'
+        self.setStyleSheet('QWidget#info_widget { ' + self.stylesheet + ' }')
+ 
         self.settings = settings
 
         self.ping_thread = None
+        self.services = {}
 
         # info panel
         self.info_widget = qt.QWidget()
@@ -150,6 +194,7 @@ class InfoWidget(qt.QWidget):
         self.layout.addWidget(self.info_widget)
 
         self.name_label = qt.QLabel(self.settings['name'])
+        self.name_label.setStyleSheet('font-weight: bold;')
         self.info_layout.addWidget(self.name_label)
 
         self.hostname_label = qt.QLabel('Hostname: ' + self.settings['hostname'])
@@ -161,6 +206,12 @@ class InfoWidget(qt.QWidget):
         self.recording_label = qt.QLabel('Recording:')
         self.info_layout.addWidget(self.recording_label)
 
+        line = qt.QFrame()
+        line.setFrameShape(qt.QFrame.HLine)
+        line.setFrameShadow(qt.QFrame.Plain)
+        line.setStyleSheet('QFrame {background-color: #cccccc; max-height: 1px; border: none; }')
+        self.layout.addWidget(line)
+
         # command panel
         self.command_widget = qt.QWidget()
         self.command_layout = qt.QHBoxLayout(self.command_widget)
@@ -171,12 +222,15 @@ class InfoWidget(qt.QWidget):
         self.command_layout.addWidget(self.refresh_button)
 
         self.restart_button = qt.QPushButton(text='Restart')
+        self.restart_button.clicked.connect(lambda :self.docker_command('restart'))
         self.command_layout.addWidget(self.restart_button)
 
         self.up_button = qt.QPushButton(text='Up')
+        self.up_button.clicked.connect(lambda :self.docker_command('up'))
         self.command_layout.addWidget(self.up_button)
 
         self.down_button = qt.QPushButton(text='Down')
+        self.down_button.clicked.connect(lambda :self.docker_command('down'))
         self.command_layout.addWidget(self.down_button)
         
         self.record_button = qt.QPushButton(text='Record')
@@ -190,6 +244,12 @@ class InfoWidget(qt.QWidget):
         self.delete_button = qt.QPushButton(text='X')
         self.delete_button.clicked.connect(self.delete_clicked)
         self.command_layout.addWidget(self.delete_button)
+        
+        line = qt.QFrame()
+        line.setFrameShape(qt.QFrame.HLine)
+        line.setFrameShadow(qt.QFrame.Plain)
+        line.setStyleSheet('QFrame {background-color: #cccccc; max-height: 1px; border: none; }')
+        self.layout.addWidget(line)
 
         # docker panel
         self.docker_widget = qt.QWidget()
@@ -197,6 +257,7 @@ class InfoWidget(qt.QWidget):
         self.layout.addWidget(self.docker_widget)
 
         self.update_info()
+        self.refresh_docker()
 
     def config_clicked(self):
         dialog = InfoConfigDialog(self.settings)
@@ -215,28 +276,107 @@ class InfoWidget(qt.QWidget):
             self.ping_thread.stop()
             del self.ping_thread
             self.ping_thread = None
-        self.ping_thread = CommandThread(['ping', self.settings['hostname']])
-        self.ping_thread.start()
-        self.ping_thread.output_signal.connect(self.handle_ping)
+        #self.ping_thread = CommandThread('ping ' + self.settings['hostname'])
+        self.ping_thread = CommandThread('HOST="' + self.settings['hostname'] + '"; while true; do OUTPUT=$(ping -c 1 -w 1 $HOST 2>&1); if echo "$OUTPUT" | grep -q "time="; then PING_TIME=$(echo "$OUTPUT" | grep -oP "time=\K[\d.]+"); echo "$PING_TIME ms"; else echo "failed"; fi; sleep 1; done', self.handle_ping)
+
+    def ssh_t(self, command):
+        ssh = 'ssh -t -o StrictHostKeyChecking=no'
+        if self.settings['enable_display']:
+            ssh += ' -X'
+
+        display = ''
+        if self.settings['enable_display']:
+            display = 'export DISPLAY=:1; '
+            
+        return 'sshpass -p "' + self.settings['password'] + '" ' + ssh + ' ' + \
+            self.settings['username'] + '@' + self.settings['hostname'] + ' "' + display + command + '"'
 
     def refresh_docker(self):
-        command = ''
-        if 'localhost' not in self.settings['hostname']:
-            command = 'ssh -t -o StrictHostKeyChecking=no', self.settings['username'] + '@' + self.settings['hostname']
-        command += 'cd ' + self.settings['path'] + '; docker compose config --services'
+        command = self.ssh_t('cd ' + self.settings['path'] + ';  docker compose --profile \'*\' config --services && echo SPLIT && docker compose ps --format {{.Service}}')
         
-        result = subprocess.run(command, capture_output=True, text=True, check=True, shell=True)
+        #result = subprocess.run(command, capture_output=True, text=True, check=True, shell=True)
+
+        self.refresh_thread = CommandThread(command, self.handle_refresh_docker, True, 5)
+
+    def handle_refresh_docker(self, stdout):
+        if 'SPLIT' not in stdout:
+            return
+        
+        services = sorted(stdout.split('SPLIT')[0].split('\n'))
+        running = stdout.split('SPLIT')[1].split('\n')
+        
+        for i in reversed(range(self.docker_layout.count())): 
+            self.docker_layout.itemAt(i).widget().setParent(None)
+        #self.prev_services = copy.deepcopy(self.services)
+        #self.services = {}
+        for service in services:
+            if service != '' and service not in self.settings['excluded_services']:
+                button = qt.QPushButton(text=service)
+                if service in running:
+                    button.setStyleSheet('background-color: lightgreen;')
+                button.setCheckable(True)
+                if service in self.services:
+                    button.setChecked(self.services[service])
+                else:
+                    self.services[service] = False
+                def get_click_function(s, b):
+                    def click_function():
+                        self.services[s] = b.isChecked()
+                    return click_function
+                button.clicked.connect(get_click_function(service, button))
+
+                def get_open_menu_function(s, b):
+                    def menu_triggered(action):
+                        if action.text() == 'bash':
+                            self.docker_exec(s, 'bash')
+                        elif action.text() == 'tmux':
+                            self.docker_exec(s, 'tmux a')
+                    
+                    def open_menu_function():
+                        menu = qt.QMenu(self)
+                        action1 = menu.addAction('bash')
+                        action2 = menu.addAction('tmux')
+                        menu.triggered.connect(menu_triggered)
+                        menu.popup(b.mapToGlobal(b.rect().topLeft()))
+                    return open_menu_function
+                
+                button.setContextMenuPolicy(3)
+                button.customContextMenuRequested.connect(get_open_menu_function(service, button))
+                
+                self.docker_layout.addWidget(button)
+
+    def docker_command(self, command):
+        services_str = ' '.join(self.get_selected_services())
+
+        logger.info('docker command: ' + command + ' ' + services_str)
+                
+        if len(services_str) > 0:
+            command = 'gnome-terminal --wait -- bash -c \'' + \
+                self.ssh_t('cd ' + self.settings['path'] + ';  docker compose ' + command + ' ' + services_str + \
+                           (' -d' if command == 'up' else '')) + '\''
+            #subprocess.Popen(command, shell=True)
+            self.docker_command_thread = CommandThread(command, lambda :self.refresh_docker(), True)
+
+    def docker_exec(self, service, command):
+        proc = 'gnome-terminal -- bash -c \'' + \
+            self.ssh_t('cd ' + self.settings['path'] + ';  docker exec -it '  + service + ' ' + command + '') + '\''
+        subprocess.Popen(proc, shell=True)
 
     def handle_ping(self, text):
-        if 'time=' in text:
-            ping = text.split('time=')[1]
-        else:
-            ping = 'can\'t ping'
-        
-        self.ping_label.setText('Ping: ' + ping)
+        if text == 'failed':
+            self.setStyleSheet('QWidget#info_widget { ' + self.stylesheet + 'background-color: rgb(255, 144, 144) }')
+            
+        self.ping_label.setText('Ping: ' + text)
 
     def get_dct(self):
         return self.settings
+    
+    def get_selected_services(self):
+        selected_services = []
+        for service, selected in self.services.items():
+            if selected:
+                selected_services.append(service)
+        return selected_services
         
     
 class AirstackControlPanel(Plugin):
@@ -247,6 +387,9 @@ class AirstackControlPanel(Plugin):
         self.context = context
         # to access ros2 node use self.context.node
 
+        global logger
+        logger = self.context.node.get_logger()
+
         self.info_widgets = []
 
         self.widget = qt.QWidget()
@@ -254,7 +397,13 @@ class AirstackControlPanel(Plugin):
 
         self.info_widget = qt.QWidget()
         self.info_layout = qt.QVBoxLayout(self.info_widget)
-        self.layout.addWidget(self.info_widget)
+        
+        self.info_scroll_area = qt.QScrollArea()
+        self.info_scroll_area.setWidget(self.info_widget)
+        self.info_scroll_area.setWidgetResizable(True)
+        self.info_scroll_area.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.info_scroll_area.setMinimumHeight(800)
+        self.layout.addWidget(self.info_scroll_area)
 
         self.add_button = qt.QPushButton('+')
         self.add_button.clicked.connect(lambda x:self.add_info_widget())
@@ -268,6 +417,7 @@ class AirstackControlPanel(Plugin):
             info_widget = InfoWidget()
         else:
             info_widget = InfoWidget(settings)
+        
         def get_delete_function(i):
             def delete_function():
                 self.info_layout.removeWidget(self.info_widgets[i])
@@ -282,12 +432,15 @@ class AirstackControlPanel(Plugin):
         self.info_widgets[index] = None
 
     def save_settings(self, plugin_settings, instance_settings):
-        info_dcts = [w.get_dct() for w in self.info_widgets if w != None]
+        info_dcts = [copy.deepcopy(w.get_dct()) for w in self.info_widgets if w != None]
+        for settings in info_dcts:
+            settings['password'] = xor_encrypt_decrypt(settings['password'])
         instance_settings.set_value('info_dcts', info_dcts)
 
     def restore_settings(self, plugin_settings, instance_settings):
         info_dcts = instance_settings.value('info_dcts', {})
         for settings in info_dcts:
+            settings['password'] = xor_encrypt_decrypt(settings['password'])
             self.add_info_widget(settings)
 
     def trigger_configuration(self):
--- plot/plot.py
+++ plot/plot.py
@@ -17,39 +17,62 @@ def plot(bag_path, odom_topic_name, tracking_point_topic_name):
     type_map = {topic.name: topic.type for topic in topic_types}
     msg_type = get_message(type_map[odom_topic_name])
 
+    start_time = None
+    
     odoms = []
+    odom_times = []
     tps = []
+    tp_times = []
     yaws = []
     while reader.has_next():
         topic, data, _ = reader.read_next()
         if topic == odom_topic_name:
             msg = deserialize_message(data, get_message(type_map[odom_topic_name]))
-            odoms.append([msg.pose.pose.position.x, msg.pose.pose.position.y])
+            odoms.append([msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z])
+
+            t = msg.header.stamp.sec + msg.header.stamp.nanosec*1e-9
+            if start_time == None:
+                start_time = t
+            odom_times.append(t - start_time)
             
             q = msg.pose.pose.orientation
             yaw = math.atan2(2.0 * (q.w * q.z + q.x * q.y), 1.0 - 2.0 * (q.y * q.y + q.z * q.z))
             yaws.append(yaw)
         elif topic == tracking_point_topic_name:
             msg = deserialize_message(data, get_message(type_map[tracking_point_topic_name]))
-            tps.append([msg.pose.position.x, msg.pose.position.y])
+            tps.append([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z])
+
+            t = msg.header.stamp.sec + msg.header.stamp.nanosec*1e-9
+            if start_time == None:
+                start_time = t
+            tp_times.append(t - start_time)
             
 
     odoms = np.array(odoms)
     tps = np.array(tps)
     
-    plt.figure()
-    plt.plot(odoms[:, 0], odoms[:, 1], marker='.', linestyle='-', label='odom')
-    plt.plot(tps[:, 0], tps[:, 1], marker='.', linestyle='-', label='tracking point')
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax2 = fig.add_subplot(212)
+    ax1.plot(odoms[:, 0], odoms[:, 1], marker='.', linestyle='-', label='odom')
+    ax1.plot(tps[:, 0], tps[:, 1], marker='.', linestyle='-', label='tracking point')
+    
+    ax2.plot(odom_times, odoms[:, 2], linestyle='-', label='odom')
+    ax2.plot(tp_times, tps[:, 2], linestyle='-', label='tracking point')
 
     for i, yaw in enumerate(yaws):
         x = odoms[i, 0]
         y = odoms[i, 1]
-        plt.arrow(x, y, 0.3 * math.cos(yaw), 0.3 * math.sin(yaw), head_width=0.05, head_length=0.05, fc='r', ec='r')
+        ax1.arrow(x, y, 0.3 * math.cos(yaw), 0.3 * math.sin(yaw), head_width=0.05, head_length=0.05, fc='r', ec='r')
     
-    plt.xlabel('X Position (m)')
-    plt.ylabel('Y Position (m)')
-    plt.axis('equal')
-    plt.legend()
+    ax1.set_xlabel('X Position (m)')
+    ax1.set_ylabel('Y Position (m)')
+    ax1.axis('equal')
+    ax1.legend()
+    
+    ax2.set_xlabel('time (seconds)')
+    ax2.set_ylabel('Z Position (m)')
+    ax2.legend()
     plt.show()
 
 if __name__ == "__main__":
