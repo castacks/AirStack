8b5529e44a19059261690dfcee0eaa5c5b300bc7
Kavin Kailash Ravie <kravie@andrew.cmu.edu>
2025-04-15 12:33:57 -0400

    add copyright notice

--- robot/ros_ws/src/autonomy/3_local/a_world_models/disparity_graph_cost_map/src/disparity_graph_cost_map.cpp
+++ robot/ros_ws/src/autonomy/3_local/a_world_models/disparity_graph_cost_map/src/disparity_graph_cost_map.cpp
@@ -1,3 +1,23 @@
+// Copyright (c) 2024 Carnegie Mellon University
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <disparity_graph_cost_map/disparity_graph_cost_map.hpp>
 namespace disparity_graph_cost_map {
 DisparityGraphCostMap::DisparityGraphCostMap() : CostMapInterface(), disp_graph() {
@@ -233,4 +253,4 @@ const visualization_msgs::msg::MarkerArray& DisparityGraphCostMap::get_debug_mar
 #include <pluginlib/class_list_macros.hpp>
 
 PLUGINLIB_EXPORT_CLASS(disparity_graph_cost_map::DisparityGraphCostMap,
-                       cost_map_interface::CostMapInterface)
+                       cost_map_interface::CostMapInterface) 
