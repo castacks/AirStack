429166e69c557600bb60b661b4c09ed58cf84808
John Keller <jkeller2@andrew.cmu.edu>
2025-05-16 15:48:51 -0400

    working on plotting, changing topic names for zed disparity to match real robot

--- plot/autoplot.py
+++ plot/autoplot.py
@@ -11,6 +11,68 @@ import yaml
 import utm
 import glob
 
+
+class CustomArrayContainer:
+    def __init__(self):
+        self.standalones = []
+        self.standalones_backup = []
+        self.members = []
+        self.members_backup = []
+
+    def add_standalone(self, name):
+        self.standalones.append(name)
+
+    def add_member(self, obj, field):
+        self.members.append((obj, field))
+
+    def slice(self, start_time, end_time, context):
+        self.members_backup = []
+        for obj, field in self.members:
+            a = getattr(obj, field)
+            self.members_backup.append(a)
+            setattr(obj, field, a[np.where(np.logical_and(a.bag_times >= start_time, a.bag_times <= end_time))])
+
+        self.standalones_backup = []
+        for k in self.standalones:
+            a = context[k]
+            self.standalones_backup.append((k, a.copy()))
+            context[k] = a[np.where(np.logical_and(a.bag_times >= start_time, a.bag_times <= end_time))]
+
+    def unslice(self, context):
+        for i, (obj, field) in enumerate(self.members):
+            setattr(obj, field, self.members_backup[i])
+
+        for k, a in self.standalones_backup:
+            context[k] = a
+
+custom_array_container = CustomArrayContainer()
+
+custom_standalone_arrays = []
+custom_arrays = []
+custom_arrays_backup = []
+custom_standalone_arrays_backup = []
+
+def slice_arrays(start_time, end_time, context):
+    global custom_arrays, custom_standalone_arrays, custom_arrays_backup, custom_standalone_arrays_backup
+    custom_arrays_backup = []
+    for obj, field in custom_arrays:
+        a = getattr(obj, field)
+        custom_arrays_backup.append(a)
+        setattr(obj, field, a[np.where(np.logical_and(a.bag_times >= start_time, a.bag_times <= end_time))])
+
+    for k in custom_standalone_arrays:
+        a = context[k]
+        custom_standalone_arrays_backup.append((k, a.copy()))
+        context[k] = a[np.where(np.logical_and(a.bag_times >= start_time, a.bag_times <= end_time))]
+
+def unslice_arrays(context):
+    global custom_arrays, custom_standalone_arrays, custom_arrays_backup, custom_standalone_arrays_backup
+    for i, (obj, field) in enumerate(custom_arrays):
+        setattr(obj, field, custom_arrays_backup[i])
+
+    for k, a in custom_standalone_arrays_backup:
+        context[k] = a
+
 def get_time(stamp):
     return stamp.sec + stamp.nanosec*1e-9
 
@@ -19,6 +81,10 @@ class CustomArray(np.ndarray):
         obj = np.asarray(input_array).view(cls)
         obj.bag_times = None
         return obj
+
+    def __get_attribute__(self, name):
+        print('name', name)
+        return super().__getattribute__(name)
 
     def __array_finalize__(self, obj):
         if obj is None:
@@ -31,15 +97,6 @@ def make_list(o):
     else:
         return [o]
 
-def get_data_and_bag_times(s, extra_context={}):
-    x = eval(s, globals(), extra_context)
-    if hasattr(x, 'bag_times'):
-        return x, x.bag_times
-    elif type(x) == tuple:
-        return x[0], x[1].bag_times
-    else:
-        return x, None
-
 def apply_function(variable, function_name, function_arguments, extra_context={}):
     if function_name not in dir(variable):
         print('warning: ' + function_name + ' not a member of ' + str(type(variable)) + '. skipping.')
@@ -47,11 +104,13 @@ def apply_function(variable, function_name, function_arguments, extra_context={}
     try:
         context = {'variable': variable}
         context.update(extra_context)
-        eval('variable.' + function_name + '(' + function_arguments + ')', globals(), context)
-    except:
+        eval('variable.' + function_name + '(' + (function_arguments if function_arguments != None else '') + ')',
+             globals(), context)
+    except Exception as e:
         context = {'variable': variable}
         context.update(extra_context)
-        eval('variable.' + function_name + '(\'' + function_arguments + '\')', globals(), context)
+        eval('variable.' + function_name + '(\'' + (function_arguments if function_arguments != None else '') + '\')',
+             globals(), context)
 
 def apply_profiles(o, profiles, keys):
     combined_profile = {}
@@ -111,7 +170,7 @@ def load_functions(functions):
             return func
         context[name] = get_func()
 
-def create_object(msgs, bag_times):
+def create_object(msgs, bag_times, input_field=''):
     if len(msgs) == 0:
         a = CustomArray([])
         a.bag_times = np.array([])
@@ -119,6 +178,7 @@ def create_object(msgs, bag_times):
     
     first = msgs[0]
 
+    #print('field', input_field, hasattr(first, '_fields_and_field_types'))
     if not hasattr(first, '_fields_and_field_types'):
         a = CustomArray(msgs)
         a.bag_times = bag_times
@@ -129,8 +189,10 @@ def create_object(msgs, bag_times):
 
     for field in fields:
         values = [getattr(msg, field) for msg in msgs]
-        o = create_object(values, bag_times)
+        o = create_object(values, bag_times, field)
         setattr(obj, field, o)
+        if type(o) == CustomArray:
+            custom_array_container.add_member(obj, field)
     return obj
         
 def load_data(bag_path, data_map):
@@ -186,18 +248,45 @@ def load_data(bag_path, data_map):
     for k,v in data.items():
         if len(v) == 0:
             keys_to_delete.append(k)
-        o = create_object(v, np.array(data_bag_times[k]))
+        o = create_object(v, np.array(data_bag_times[k]), 'TOP ---------------------------------------- ' + k)
         if hasattr(o, 'header'):
             o.header_time = o.header.stamp.sec + o.header.stamp.nanosec*1e-9
+            custom_array_container.add_member(o, 'header_time')
         data[k] = o
 
     for k in keys_to_delete:
         del data[k]
                 
     return data
-            
+
+class PlotInput:
+    def __init__(self):
+        self.dct = {}
+
+    def on_click(self, event):
+        if not event.dblclick:
+            return
+
+        fullscreen_pos = [0.1, 0.1, 0.8, 0.8]
         
-def plot(yaml_filename, bag_path):
+        fig = event.canvas.figure
+        if fig not in self.dct:
+            self.dct[fig] = {'fullscreen_ax': None, 'positions': [ax.get_position() for ax in fig.get_axes()]}
+
+        if self.dct[fig]['fullscreen_ax'] == None:
+            self.dct[fig]['fullscreen_ax'] = event.inaxes
+            event.inaxes.set_position(fullscreen_pos)
+            for ax in fig.get_axes():
+                if ax != event.inaxes:
+                    ax.set_visible(False)
+        else:
+            self.dct[fig]['fullscreen_ax'] = None
+            for i, ax in enumerate(fig.get_axes()):
+                ax.set_position(self.dct[fig]['positions'][i])
+                ax.set_visible(True)
+        fig.canvas.draw_idle()
+        
+def plot(yaml_filename, bag_paths):
     with open(yaml_filename, 'r') as f:
         y = yaml.safe_load(f)
         figs = y['figures']
@@ -209,9 +298,6 @@ def plot(yaml_filename, bag_path):
                 return {'key': k, 'kwargs': kwargs}
             return profile_context_function
         profiles_context = {k:get_profile_context_function(k) for k in profiles}
-        #for k, v in profiles_context.items():
-        #    print('key', k)
-        #    print('value', v())
         data_map = {}
         for name, text in d.items():
             dot_split = text.split('.', 1)
@@ -222,17 +308,27 @@ def plot(yaml_filename, bag_path):
                 field = ''
             data_map[name] = {'topic': topic, 'field': field}
     
-    data = load_data(bag_path, data_map)
-    for k, v in data.items():
-        exec('global ' + k + '\n' + k + ' = data[k]', globals(), locals())
+    custom_context = {}
+    for bag_path in bag_paths:
+        data = load_data(bag_path, data_map)
+        for k, v in data.items():
+            if k not in custom_context:
+                custom_context[k] = v
+                if type(v) == CustomArray:
+                    custom_array_container.add_standalone(k)
         
     plt.rcParams.update(rcparams)
 
+    plot_input = PlotInput()
+
     for fig in figs:
-        fig_context = {}
+        current_context = custom_context.copy()
         if 'profiles' in fig:
+            # handle fig profiles
             try:
-                fig_context = apply_profiles(fig, profiles, make_list(eval(fig['profiles'], globals(), profiles_context)))
+                temp_profiles_context = profiles_context.copy()
+                temp_profiles_context.update(current_context)
+                current_context.update(apply_profiles(fig, profiles, make_list(eval(fig['profiles'], globals(), temp_profiles_context))))
             except Exception as e:
                 print('skipping ' + str(fig) + ' because ' + str(e))
                 continue
@@ -240,15 +336,19 @@ def plot(yaml_filename, bag_path):
         # configure splitting
         split_bag_times = [0]
         if 'split' in fig:
-            try:
-                field = eval(fig['split']['field'])
-                for i, x in enumerate(field):
-                    if eval(fig['split']['condition']):
-                        split_bag_times.append(field.bag_times[i])
-            except Exception as e:
-                print('skipping split field "' + fig['split']['field'] + '" because ' + str(e))
+            for criteria in fig['split']['critera']:
+                try:
+                    field = eval(criteria['field'], globals(), current_context)
+                    for i, x in enumerate(field):
+                        # TODO don't allow x, i to be used as field names in data section
+                        current_context['x'] = x
+                        current_context['i'] = i
+                        if eval(criteria['condition'], globals(), current_context):
+                            split_bag_times.append(field.bag_times[i])
+                except Exception as e:
+                    print('skipping split field "' + criteria['field'] + '" because ' + str(e))
         split_bag_times.append(10**100)
-        split_bag_times = np.array(split_bag_times)
+        split_bag_times = np.array(sorted(split_bag_times))
 
         # figure out whether to split as figures or axes
         split_as_figures = True
@@ -284,25 +384,35 @@ def plot(yaml_filename, bag_path):
         
         if not split_as_figures:
             f = plt.figure()
+            f.canvas.mpl_connect('button_press_event', plot_input.on_click)
         for split_index in range(1, len(split_bag_times)):
             if split_as_figures:
                 f = plt.figure()
+                f.canvas.mpl_connect('button_press_event', plot_input.on_click)
             
             start_time = split_bag_times[split_index-1]
             end_time = split_bag_times[split_index]
-
+            
+            if split_as_figures:
+                custom_array_container.slice(start_time, end_time, current_context)
+            
             # call functions
             for k in fig:
                 if k not in ['axes', 'profiles']:
-                    apply_function(f, k, fig[k], fig_context)
+                    apply_function(f, k, fig[k], current_context)
+            
+            if not split_as_figures:
+                custom_array_container.slice(start_time, end_time, current_context)
 
             total_rows, total_cols = get_row_col_info(fig['axes'])
 
             for ax in fig['axes']:
-                # handle profiles
-                ax_context = fig_context.copy()
+                # handle ax profiles
+                ax_context = current_context.copy()
                 if 'profiles' in ax:
-                    ax_context.update(apply_profiles(ax, profiles, make_list(eval(ax['profiles'], globals(), profiles_context))))
+                    temp_profiles_context = profiles_context.copy()
+                    temp_profiles_context.update(current_context)
+                    ax_context.update(apply_profiles(ax, profiles, make_list(eval(ax['profiles'], globals(), temp_profiles_context))))
 
                 # configure subplots
                 if not split_as_figures:
@@ -330,21 +440,19 @@ def plot(yaml_filename, bag_path):
                     args = []
                     # configure x
                     if 'x' in plot.keys():
-                        x, x_bag_times = get_data_and_bag_times(plot['x'], ax_context)
                         try:
-                            x = x[np.where(np.logical_and(x_bag_times >= start_time, x_bag_times <= end_time))]
+                            x = eval(plot['x'], globals(), ax_context)
                         except Exception as e:
-                            #print(e)
-                            pass
+                            print(f'error in x ({plot["x"]}): {e}. skipping {plot}')
+                            continue
                         args.append(x)
                         
                     # configure y
-                    y, y_bag_times = get_data_and_bag_times(plot['y'], ax_context)
                     try:
-                        y = y[np.where(np.logical_and(y_bag_times >= start_time, y_bag_times <= end_time))]
+                        y = eval(plot['y'], globals(), ax_context)
                     except Exception as e:
-                        #print(e)
-                        pass
+                        print(f'error in y ({plot["y"]}): {e}. skipping {plot}')
+                        continue
                     args.append(y)
 
                     # configure args
@@ -356,12 +464,10 @@ def plot(yaml_filename, bag_path):
                     a.plot(*args, **kwargs)
                     if 'label' in kwargs:
                         a.legend()
-                
+            custom_array_container.unslice(current_context)
     plt.show()
         
 if __name__ == "__main__":
-    yaml_filename = sys.argv[1]    
-    for i in range(2, len(sys.argv)):
-        bag_path = sys.argv[i]
-        print(bag_path)
-        plot(yaml_filename, bag_path)
+    yaml_filename = sys.argv[1]
+    bag_paths = sys.argv[2:]
+    plot(yaml_filename, bag_paths)
