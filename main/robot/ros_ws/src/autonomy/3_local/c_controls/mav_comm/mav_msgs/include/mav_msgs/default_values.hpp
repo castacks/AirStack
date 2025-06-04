948be31093cc33e8a9741c495492e8265f171422
Docs Deploy <docs.deploy@example.co.uk>
2025-06-04 22:54:27 +0000

    Deployed 6756bea to main with MkDocs 1.6.1 and mike 2.1.3

--- main/robot/ros_ws/src/autonomy/3_local/c_controls/mav_comm/mav_msgs/include/mav_msgs/default_values.hpp
+++ main/robot/ros_ws/src/autonomy/3_local/c_controls/mav_comm/mav_msgs/include/mav_msgs/default_values.hpp
@@ -1,3 +1,23 @@
+// Copyright (c) 2024 Carnegie Mellon University
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 /*
  * Copyright 2015 Fadri Furrer, ASL, ETH Zurich, Switzerland
  * Copyright 2015 Michael Burri, ASL, ETH Zurich, Switzerland
@@ -30,4 +50,4 @@ const double kZurichHeight = 405.94;
 const double kGravity = MagnitudeOfGravity(kZurichHeight, kZurichLatitude);
 }  // namespace mav_msgs
 
-#endif /* MAV_MSGS_DEFAULT_VALUES_H_ */
+#endif /* MAV_MSGS_DEFAULT_VALUES_H_ */ 
